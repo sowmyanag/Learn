fb2a8c534f9047d62967c7109f477fb0dd4a58cf
Abhishek <Abhishek>
2016-05-16 22:54:56 +0530

    Comitting with all files

---
+++ src/main/java/abhishek/learn/all/ngMvn/App.java
@@ -0,0 +1,13 @@
+package abhishek.learn.all.ngMvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/abhishek/learn/all/ngMvn/NewTest.java
@@ -0,0 +1,24 @@
+package abhishek.learn.all.ngMvn;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.println("Voila!!!");
+	  
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  System.out.println("Before test");
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  System.out.println("After test");
+  }
+
+  
+}
