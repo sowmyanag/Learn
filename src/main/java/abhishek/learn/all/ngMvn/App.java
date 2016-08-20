b310b4f853f45fc85f136181ffdda4706335b4e6
Abhishek <Abhishek>
2016-08-20 19:16:02 +0530

    Removed Voila

--- src/main/java/abhishek/learn/all/ngMvn/App.java
+++ src/main/java/abhishek/learn/all/ngMvn/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!!Voila!!" );
+        System.out.println( "Hello World!!" );
     }
 }
